ce7c64bce09f88f69dee9025c42d21987d0bb580
James Hill <j.g.hill@zoho.com>
2018-03-31 04:18:03 +0100

    Amended all code to match PEP8 styling

--- Audio Tests/Basic_Sound_Processing.py
+++ Audio Tests/Basic_Sound_Processing.py
@@ -1,9 +1,8 @@
+import librosa as lb
 import numpy as np
+import os
+import pylab as pl
 import soundfile as sf
-
-from librosa import*
-from librosa import display
-from pylab import*
 
 
 def soundInfo(fileName, series, sampRate):
@@ -12,77 +11,79 @@ def soundInfo(fileName, series, sampRate):
     print('Data type              : ' + str(series.dtype))
     print('Sample points          : ' + str(series.shape[0]))
     print('Sample rate            : ' + str(sampRate))
-    print('Duration               : ' + str(core.get_duration(series, sampRate)))
+    print('Duration               : ' +
+          str(lb.core.get_duration(series, sampRate)))
     print('\n')
     return
 
 
-def plotTone(s, sf):
+def plotTone(series, sampRate):
     """ Plot a tone to a graph """
     # Convert sound array to floating point between -1 to 1
-    snd = s / (2.**15)
-    timeArray = arange(0, snd.shape[0])
-    timeArray = timeArray / sf
-    timeArray = timeArray * 1000 # scales to milliseconds
+    snd = series / (2. ** 15)
+    timeArray = pl.arange(0, snd.shape[0])
+    timeArray = timeArray / sampRate
+    timeArray = timeArray * 1000  # scales to milliseconds
     # Plot the tone graph
-    plot(timeArray, snd, color='k')
-    ylabel('Amplitude')
-    xlabel('Time (ms)')
-    plt.show()
+    np.plot(timeArray, snd, color='k')
+    np.ylabel('Amplitude')
+    np.xlabel('Time (ms)')
+    np.plt.show()
     return
 
 
-def plotFrequency(s, sf):
+def plotFrequency(series, sampRate):
     """ Plot a frequency to a graph """
-    s = s / (2.**15)
-    p, freqArray = _fastFourier(s, sf)
-    plot(freqArray/1000, 10*log10(p), color='k')
-    xlabel('Frequency (kHz)')
-    ylabel('Power (dB)')
-    plt.show()
+    s = series / (2. ** 15)
+    p, freqArray = _fastFourier(s, sampRate)
+    np.plot(freqArray / 1000, 10 * pl.log10(p), color='k')
+    np.xlabel('Frequency (kHz)')
+    np.ylabel('Power (dB)')
+    np.plt.show()
     return
 
 
-def compareRoots(s, sf):
+def compareRoots(series, sampRate):
     """ Compare the roots to see that they are equal """
-    s = s / (2.**15)
-    p, freqArray = _fastFourier(s, sf)
-    rms_val = sqrt(mean(s**2))
+    s = series / (2. ** 15)
+    p, freqArray = _fastFourier(s, sampRate)
+    rms_val = pl.sqrt(pl.mean(s ** 2))
     print('Root mean square value : ' + str(rms_val))
-    print('Square root            : ' + str(sqrt(sum(p))))
+    print('Square root            : ' + str(pl.sqrt(sum(p))))
     return
 
 
-def _fastFourier(s, sf):
+def _fastFourier(series, sampRate):
     """ Perform a Fast Fourier Transform """
-    n = len(s)
-    p = fft(s) # Fast fourier transform
-    uUniquePts = int(ceil((n+1)/2.0))
+    n = len(series)
+    p = sf.fft(series)  # Fast fourier transform
+    uUniquePts = int(pl.ceil((n+1) / 2.0))
     p = p[0:uUniquePts]
     p = abs(p)
     p = p / float(n)
-    p = p**2
+    p = p ** 2
     if n % 2 > 0:
         p[1:len(p)] = p[1:len(p)] * 2
     else:
         p[1:len(p) - 1] = p[1:len(p) - 1] * 2
-        
-    freqArray = arange(0, uUniquePts, 1.0) * (sf / n);
+    freqArray = np.arange(0, uUniquePts, 1.0) * (sampRate / n)
     return (p, freqArray)
 
 
 def showWaveplot(series, sampRate):
-    display.waveplot(series, sampRate)
-    plt.show()
+    """ Plots the wave of the sound """
+    pl.display.waveplot(series, sampRate)
+    pl.plt.show()
     return
 
 
 def copyDownSampleFolder(inFilePath, folderName, outFilePath, newSampRate):
-    files = util.find_files(inFilePath + folderName + '/', ext='wav')
+    """ Copies folder of samples to a new sample rate """
+    files = lb.util.find_files(inFilePath + folderName + '/', ext='wav')
     for file in files:
         path, name = os.path.split(file)
-        series, sampRate = core.load(file, sr=None)
-        reSampled = core.resample(series, sampRate, newSampRate)
+        series, sampRate = lb.core.load(file, sr=None)
+        reSampled = lb.core.resample(series, sampRate, newSampRate)
         sf.write(
             file=outFilePath + folderName + '/' + name,
             data=reSampled,
@@ -91,17 +92,19 @@ def copyDownSampleFolder(inFilePath, folderName, outFilePath, newSampRate):
     return
 
 
-inFilePath    = '/home/zhanmusi/Documents/Data/Speech Commands Dataset/'
-inFileFolder  = 'zero'
-inFileName    = '0ab3b47d_nohash_0.wav'
-inFullPath    = inFilePath + inFileFolder + '/' + inFileName
+inFilePath = '/home/zhanmusi/Documents/Data/Speech Commands Dataset/'
+inFileFolder = 'zero'
+inFileName = '0ab3b47d_nohash_0.wav'
+inFullPath = inFilePath + inFileFolder + '/' + inFileName
 
-outFilePath   = '/home/zhanmusi/Documents/Data/Speech Commands Dataset Downsampled/'
+outFilePath = \
+    '/home/zhanmusi/Documents/Data/Speech Commands Dataset Downsampled/'
 outFileFolder = 'zero'
-outFileName   = 'downsampled_' + inFileName
-outFullPath   = outFilePath + outFileFolder + '/' + outFileName
+outFileName = 'downsampled_' + inFileName
+outFullPath = outFilePath + outFileFolder + '/' + outFileName
 
-series, sampRate = core.load(inFullPath, sr=None)
-reSampled = core.resample(series, sampRate, int(sampRate/2))
+series, sampRate = lb.core.load(inFullPath, sr=None)
+reSampled = lb.core.resample(series, sampRate, int(sampRate / 2))
+
 
 # exec(open('Basic_Sound_Processing.py').read())
