64c467b15345d4271c307ba4e1da7dd948ee6026
James Hill <j.g.hill@zoho.com>
2018-06-27 20:41:59 +0100

    Added function explanation, corrected path errors
    Former-commit-id: 898608f3472699d23fea5d4ffc41d0c3c42437b9

--- Audio Manipulation/Downsampler.py
+++ Audio Manipulation/Downsampler.py
@@ -15,21 +15,24 @@ folders = ['zero', 'one']
 
 
 def main(divideBy):
+    """ Runs the code """
     global newSampleRate
     newSampleRate = sampleRate / divideBy
-    fullOutPath = outPath + newSampleRate + '/'
-    loopFolders(fullOutPath)
+    loopFolders()
     return
 
 
-def loopFolders(fullOutPath):
-    for folder in fullOutPath:
-        loopFiles(folder)
+def loopFolders():
+    """ Loops through all folders found at the inPath """
+    for folder in folders:
+        path = inPath + folder + '/'
+        loopFiles(path)
     return
 
 
 def loopFiles(folder):
-    allFiles = lb.util.find_files(inPath + folder + '/', ext='wav')
+    """ Loop through all files found within the folder  """
+    allFiles = lb.util.find_files(folder, ext='wav')
     for eachFile in allFiles:
         filePath, fileName = os.path.split(eachFile)
         resampled = resampleFile(eachFile)
@@ -38,10 +41,17 @@ def loopFiles(folder):
 
 
 def resampleFile(wav):
-    return
+    """ Resample the wav file passed to the function """
+    series, sampRate = lb.core.load(wav, sr=None)
+    resampled = lb.core.resample(
+        y=series,
+        orig_sr=sampRate,
+        target_sr=newSampleRate)
+    return resampled
 
 
 def saveFile(folder, resampled, fileName):
+    """ Save the file """
     sf.write(
         file=outPath + folder + '/' + fileName,
         data=resampled,
@@ -51,7 +61,7 @@ def saveFile(folder, resampled, fileName):
 
 
 if __name__ == "__main__":
-    parser = ag.ArgumentParser(description="Pass the downsample divider:")
+    parser = ag.ArgumentParser(description="Pass a downsample divider:")
     parser.add_argument(
         'divider',
         type=int,
