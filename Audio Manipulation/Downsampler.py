969a46738bd3f75cb04a8a0c4053a067a1e1318b
James Hill <j.g.hill@zoho.com>
2018-06-27 18:51:26 +0100

    Added main, imports, variables, most functions
    Former-commit-id: adb1da035a28f3d1651e6fe25e68dd806ce13edf

--- Audio Manipulation/Downsampler.py
+++ Audio Manipulation/Downsampler.py
@@ -1,20 +1,53 @@
+import librosa as lb
 import os
+import soundfile as sf
 
 
+# The paths for loading & saving the .wav files
 inPath = '/home/zhanmusi/Documents/Data/Speech Commands Dataset/'
 outPath = '/home/zhanmusi/Documents/Data/Speech Commands Dataset Downsampled/'
 
+# List of folders the word recordings should be extracted from
+folders = ['zero', 'one']
 
-def loopFolders(path):
-    folders = os.listdir(path)
+# divideBy can be changed to determine the downsample rate
+divideBy = 4
+sampleRate = 16000
+newSampleRate = sampleRate / divideBy
+
+
+def main():
+    loopFolders()
+    return
+
+
+def loopFolders():
     for folder in folders:
         loopFiles(folder)
     return
 
 
-def loopFiles(path):
+def loopFiles(folder):
+    allFiles = lb.util.find_files(inPath + folder + '/', ext='wav')
+    for eachFile in allFiles:
+        filePath, fileName = os.path.split(eachFile)
+        resampled = resampleFile(eachFile)
+        saveFile(folder, resampled, fileName)
     return
 
 
 def resampleFile(wav):
     return
+
+
+def saveFile(folder, resampled, fileName):
+    sf.write(
+        file=outPath + folder + '/' + fileName,
+        data=resampled,
+        samplerate=newSampleRate,
+        subtype='PCM_16')
+    return
+
+
+if __name__ == "__main__":
+    main()
