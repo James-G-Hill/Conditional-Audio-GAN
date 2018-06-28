82ca48d16a9dc8c5fb1064e4853dc9186d84456c
James Hill <j.g.hill@zoho.com>
2018-06-28 21:05:42 +0100

    Created initial method names
    Former-commit-id: 1b0cb89f038219b620a3a1ee12abeeb07e3c9e58

---
+++ Initial Testing/Testing Discriminator.py
@@ -0,0 +1,13 @@
+def loadData():
+    """ Loads the audio data into list form """
+    return
+
+
+def trainDiscriminator():
+    """ Trains the discriminator """
+    return
+
+
+def waveGANdiscriminator():
+    """ A discriminator for a WaveGAN Model """
+    return
