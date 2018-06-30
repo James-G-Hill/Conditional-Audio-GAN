a1d9cbad898809886bd72db7d4b36d4cfc414a77
James Hill <j.g.hill@zoho.com>
2018-06-30 17:55:33 +0100

    Added code for trainDiscriminator function + load module function
    Former-commit-id: d4160ec04e68c258cf9b57ac638ff9575275eb5f

--- Initial Testing/Testing Discriminator.py
+++ Initial Testing/Testing Discriminator.py
@@ -1,17 +1,30 @@
-def loadData(folderNames):
-    """ Loads the audio data into list form """
-
-    return
+import importlib.util as util
+import tensorflow as tf
 
 
-def trainDiscriminator():
+def trainDiscriminator(inPath, folders, stepCount):
     """ Trains the discriminator """
+    audioLoader = _loadModule(
+        'audioDataLoader',
+        '/home/zhanmusi/Documents/Data/' +
+        'Speech Commands Dataset Downsampled 2048')
+    data, labels, lookup = audioLoader(inPath, folders)
+    waveGANestimator = tf.estimator.Estimator(
+        model_fn=waveGANdiscriminator,
+        model_dir='tmp/testWaveGANDiscriminator')
+    waveGANestimator.train(
+        input_fn=,
+        steps=stepCount)
     return
 
 
-def waveGANdiscriminator():
+def _loadModule(modName, modPath):
+    """ Loads a module from file location """
+    spec = util.spec_from_file_location(modName, modPath)
+    mod = util.module_from_spec(spec)
+    return mod
+
+
+def _waveGANdiscriminator():
     """ A discriminator for a WaveGAN Model """
     return
-
-
-d
