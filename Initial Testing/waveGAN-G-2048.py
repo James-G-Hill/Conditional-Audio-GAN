0605ed5887a0c57c6e415738dcba67f43049c4c3
James Hill <j.g.hill@zoho.com>
2018-07-08 01:12:39 +0100

    Created new generator network file
    Former-commit-id: eaf61f55f9ebe39093d37e54f6b0e1eb77f7e4c7

---
+++ Initial Testing/waveGAN-G-2048.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+
+BATCH_SIZE = 64
+CHANNELS = 1
+KERNEL_SIZE = 25
+MODEL_SIZE = 16
+STRIDE = [1, 4, 1]
+WAV_LENGTH = 1024
+Z_LENGTH = 100
+
+
+def generate(z_input):
+    """ A waveGAN generator """
+
+    # Input: [64, 100] > [64, 1024]
+    densify = tf.layers.dense(
+        inputs=z_input,
+        units=[Z_LENGTH * WAV_LENGTH],
+        name="Input Dense"
+    )
+
+    # Input: [64, 1024] > [64, 16, 64]
+    shape = tf.reshape(
+        tensor=tf.cast(densify, tf.float32),
+        shape=[BATCH_SIZE, MODEL_SIZE, MODEL_SIZE * 4]
+    )
+
+    # Input: [64, 16, 64] > [64, 64, 32]
+    trans_conv_1 = tf.contrib.nn.conv1_transpose(
+        value=shape,
+        filter=[KERNEL_SIZE, MODEL_SIZE * 2, MODEL_SIZE * 4],
+        output_shape=MODEL_SIZE * 4,
+        stride=STRIDE,
+        padding='same',
+        name="Trans_Convolution_1"
+    )
+
+    # Input: [64, 64, 32] > [64, 256, 16]
+    trans_conv_2 = tf.contrib.nn.conv1_transpose(
+        value=trans_conv_1,
+        filter=[KERNEL_SIZE, MODEL_SIZE * 1, MODEL_SIZE * 2],
+        output_shape=MODEL_SIZE * 16,
+        stride=STRIDE,
+        padding='same',
+        name="Trans_Convolution_2"
+    )
+
+    # Input: [64, 256, 16] > [64, 1024, 1]
+    trans_conv_3 = tf.contrib.nn.conv1_transpose(
+        value=trans_conv_2,
+        filter=[KERNEL_SIZE, CHANNELS, MODEL_SIZE * 1],
+        output_shape=MODEL_SIZE * 16,
+        stride=STRIDE,
+        padding='same',
+        name="Trans_Convolution_3"
+    )
+
+    # Input: [64, 1024, 1]
+    activation_tanh = tf.tanh(
+        x=trans_conv_3,
+        name="Final Tanh"
+    )
+
+    return activation_tanh
